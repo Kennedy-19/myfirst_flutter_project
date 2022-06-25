47c3eb44f706f305f674a77b2883c30af9cea3a2
Kennedy-19 <eskokinskenny@gmail.com>
2022-06-25 23:34:07 +0300

    categorieswidget

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:myfirst_flutter_project/widgets/CategoriesWidgets.dart';
 import 'package:myfirst_flutter_project/widgets/HomeAppBar.dart';
 
 class HomePage extends StatelessWidget {
@@ -55,6 +56,23 @@ class HomePage extends StatelessWidget {
               ],
               ),
             ),
+            Container(
+              //alignment: Alignment.centerLeft'
+              margin: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 10,
+              ),
+              child: Text(
+                "Categories",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4C53A5)),
+                  ),
+            ),
+
+            //Categories
+            CategoriesWidget()
           ],
         ),
         ),
---
+++ lib/widgets/CategoriesWidgets.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("images/1.png"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
