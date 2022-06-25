d9343a9a9880adcd073c5232a3213a0a544beadc
Kennedy-19 <eskokinskenny@gmail.com>
2022-06-25 07:30:34 +0300

    homepage

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:myfirst_flutter_project/widgets/HomeAppBar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: ListView(
+      children: [
+        HomeAppBar(),
+        Container(
+          // temporary height
+          height: 500,
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color: Color(0xFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            )
+          ),
+          child: Column(children: [
+            Container(margin: EdgeInsets.symmetric(horizontal: 15),
+            
+            ),
+          ],
+        ),
+        ),
+      ],),
+      );
   }
 }
